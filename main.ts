d6c2fee5947b778bbf1b016d1649b17a8c02874c
adamalbu <45970222+adamalbu@users.noreply.github.com>
2020-08-02 09:14:27 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,88 @@
 enum ActionKind {
     Walking,
     Idle,
-    Jumping
+    Jumping,
+    Ghhghggg
 }
-function Idle () {
-    let Idle2: animation.Animation = null
-    animation.attachAnimation(Sausage, Idle2)
+function Idle_anim () {
+    Idle_var = animation.createAnimation(ActionKind.Idle, 100)
+    Idle_var.addAnimationFrame(img`
+        . . . . . . . . 4 4 . . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . f 4 4 f . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . f 4 4 f . . . . . . . 
+        . . . . . . . 4 f f 4 . . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . . f 4 4 f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        `)
+    Idle_var.addAnimationFrame(img`
+        . . . . . . . . 4 4 . . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 4 f 4 . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 4 f 4 . . . . . . . 
+        . . . . . . . f f 4 4 . . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . . f 4 4 f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        `)
+    Idle_var.addAnimationFrame(img`
+        . . . . . . . . 4 4 . . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 f 4 4 . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . 4 f 4 4 . . . . . . . 
+        . . . . . . . f 4 4 4 . . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . . . 
+        . . . . . . . f 4 4 f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        . . . . . . . f . . f . . . . . . . 
+        `)
+    for (let index = 0; index < 10; index++) {
+        Idle_var.addAnimationFrame(img`
+            . . . . . . . . 4 4 . . . . . . . . 
+            . . . . . . . 4 4 4 4 . . . . . . . 
+            . . . . . . . f 4 4 4 . . . . . . . 
+            . . . . . . . 4 4 4 4 . . . . . . . 
+            . . . . . . . 4 4 4 4 . . . . . . . 
+            . . . . . . . f 4 4 4 . . . . . . . 
+            . . . . . . . 4 4 4 4 . . . . . . . 
+            . . . . . . f 4 4 4 4 f . . . . . . 
+            . . . . . . f 4 4 4 4 f . . . . . . 
+            . . . . . . f 4 4 4 4 f . . . . . . 
+            . . . . . . f 4 4 4 4 f . . . . . . 
+            . . . . . . f 4 4 4 4 f . . . . . . 
+            . . . . . . . f 4 4 f . . . . . . . 
+            . . . . . . . f . . f . . . . . . . 
+            . . . . . . . f . . f . . . . . . . 
+            . . . . . . . f . . f . . . . . . . 
+            `)
+    }
 }
-let Sausage: Sprite = null
-Sausage = sprites.create(img`
+let Idle_var: animation.Animation = null
+let Sausage = sprites.create(img`
     . . . . . . . . 4 4 . . . . . . . . 
     . . . . . . . 4 4 4 4 . . . . . . . 
     . . . . . . . f 4 4 f . . . . . . . 
@@ -26,3 +100,5 @@ Sausage = sprites.create(img`
     . . . . . . . f . . f . . . . . . . 
     . . . . . . . f . . f . . . . . . . 
     `, SpriteKind.Player)
+Idle_anim()
+animation.setAction(Sausage, ActionKind.Idle)
